e26c0673e3d019a24a8cc487efffba56b943762e
Ítalo Guilherme De Meneses E Costa <italoguilherme@rede.ulbra.br>
2025-06-04 21:15:39 -0300

    atividade

---
+++ src/modules/coffees/coffees.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CoffeeController } from './coffees.controller';
+import { CoffeeService } from './coffees.service';
+
+@Module({
+  imports: [],
+  controllers: [CoffeeController],
+  providers: [CoffeeService],
+})
+export class CoffeesModule {}
