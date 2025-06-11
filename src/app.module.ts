c0c4b305aa8d0d31b2231feb7a70b6eb766b663b
Italo <italoguilherme.glhrm@gmail.com>
2025-06-11 20:07:51 -0300

    iniciando...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoffeesModule } from './modules/coffees/coffees.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, PrismaModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
